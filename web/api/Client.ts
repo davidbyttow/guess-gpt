13c2488437193a17a7df396e5e0f0a4e812176fa
David Byttow <1735553+davidbyttow@users.noreply.github.com>
2023-05-06 16:33:12 -0700

    setting up heroku (#1)
    * another attempt
    
    * jsut python
    
    * update
    
    * try again
    
    * remove proc
    
    * web disT
    
    * dist
    
    * use webdist
    
    * remove web-dist

--- web/api/Client.ts
+++ web/api/Client.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const client: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5001",
+  // baseURL: "http://localhost:5001",
   timeout: 1000 * 60 * 3, // 3 min, gpt requests can take a long time :)
   headers: {
     "Content-Type": "application/json",

903818f6759c810c06326bcdbe31b3101b0f8baa
David Byttow <david@sorare.com>
2023-05-05 18:15:17 -0700

    procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+server: python scripts/run.py --port=$PORT
--- scripts/run.py
+++ scripts/run.py
@@ -1,5 +1,11 @@
+import argparse
+
 from app.routes import app
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--port", type=int, default=5001)
+args = parser.parse_args()
+port = args.port
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    app.run(debug=True, host="0.0.0.0", port=port)

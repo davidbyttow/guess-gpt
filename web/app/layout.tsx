6747013940a10b0bea9269a1bef96340c67a24d0
David Byttow <david@sorare.com>
2023-05-07 18:35:46 -0400

    maximimum scale

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,10 +1,21 @@
-"use client";
-
 import Providers from "./providers";
 
+export const metadata = {
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        ></meta>
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>

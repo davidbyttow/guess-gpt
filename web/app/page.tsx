db6b2eca8682a2cfad120695729f01db4048a559
David Byttow <david@sorare.com>
2023-05-07 23:22:43 -0400

    fix redirect

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
   const router = useRouter();
-  if (typeof window !== "undefined") {
-    router.push("/create");
-  }
 
+  // TODO(d): I don't know why this was necessary and always rendering
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.location.pathname === "/") {
+      router.push("/create");
+    }
+  }, []);
   return <></>;
 }
